075fc4d72d53093de74815a7f52489295991aa52
Danil Tkachenko <d_tkachenko@utb.cz>
2022-12-17 23:52:56 +0100

    end work on regestration page and sign in page

--- Eshop/Controllers/AccountController.cs
+++ Eshop/Controllers/AccountController.cs
@@ -8,93 +8,84 @@ using Eshop.Models;
 
 namespace Eshop.Controllers
 {
-    public class AccountController : Controller
-    {
-        private readonly UserManager<IdentityUser> userManager;
-        private readonly SignInManager<IdentityUser> signInManager;
-        private readonly EshopContext _context;
-        //RoleManager<IdentityRole> roleManager;
+	public class AccountController : Controller
+	{
+		private readonly UserManager<IdentityUser> userManager;
+		private readonly SignInManager<IdentityUser> signInManager;
+		private readonly EshopContext _context;
+		//RoleManager<IdentityRole> roleManager;
 
-        public AccountController(UserManager<IdentityUser> userManager,
-            SignInManager<IdentityUser> signInManager,
-            //RoleManager<IdentityRole> roleManager,
-            EshopContext context)
-        {
+		public AccountController(UserManager<IdentityUser> userManager,
+		    SignInManager<IdentityUser> signInManager,
+		    //RoleManager<IdentityRole> roleManager,
+		    EshopContext context)
+		{
 
-            this.userManager = userManager;
-            this.signInManager = signInManager;
-            //this.roleManager=roleManager; //добавление роли 
-            this._context = context;
+			this.userManager = userManager;
+			this.signInManager = signInManager;
+			//this.roleManager=roleManager; //добавление роли 
+			this._context = context;
 
-        }
+		}
 
+		[HttpGet]
+		public ActionResult Register()
+		{
 
+			return View();
+		}
 
+		[HttpPost]
+		public async Task<IActionResult> Register(User model, string Password, string email, string phoneNumber)
+		{
 
-        [HttpGet]
-        public ActionResult Register()
-        {
+			if (ModelState.IsValid)
+			{
+				IdentityUser User = new IdentityUser { UserName = model.Name, Email = email, PhoneNumber = phoneNumber };
+				var result = await userManager.CreateAsync(User, Password);
 
-            return View();
-        }
-        [HttpPost]
-        public async Task<IActionResult> Register(User model, string Password,string email,string phoneNumber)
-        {
+				if (result.Succeeded)
+				{
+					await signInManager.SignInAsync(User, isPersistent: true);// false без сохранения
 
-            if (ModelState.IsValid)
-            {
-                IdentityUser User = new IdentityUser { UserName = model.Name ,Email= email, PhoneNumber= phoneNumber };
-                var result = await userManager.CreateAsync(User, Password);
+					Basket basket = new Basket { Name = $"{model.Name}'s basket", UserID = User.Id };
 
-                if (result.Succeeded)
-                {
-                    await signInManager.SignInAsync(User, isPersistent: true);// false без сохранения
+					_context.Add(basket);
 
-                    Basket basket = new Basket { Name = $"{model.Name}'s basket",UserID=User.Id };
-
-                    _context.Add(basket);
-
-                    await _context.SaveChangesAsync();
+					await _context.SaveChangesAsync();
 
 
 
-                    return RedirectToAction("Index", "Products");
-                }
-            }
-            return View(model);
+					return RedirectToAction("MainPage", "Products");
+				}
+			}
+			return View(model);
 
 
 
-        }
+		}
 
-        [HttpGet]
-        public ActionResult SignIn()
-        {
+		[HttpGet]
+		public ActionResult SignIn()
+		{
 
-            return View();
-        }
-        [HttpPost]
-        public async Task<IActionResult> SignIn(string Name, string Password)
-        {
+			return View();
+		}
 
-            if (ModelState.IsValid)
-            {
+		[HttpPost]
+		public async Task<IActionResult> SignIn(string Name, string Password)
+		{
+			if (ModelState.IsValid)
+			{
+				var result = await signInManager.PasswordSignInAsync(Name, Password, true, false);
+				if (result.Succeeded)
+				{
+					return RedirectToAction("MainPage", "Products");
 
-                var result = await signInManager.PasswordSignInAsync(Name, Password, true, false);
-                if (result.Succeeded)
-                {
-                    return RedirectToAction("Index", "Products");
-
-                }
-
-
-            }
-            return View();
-
-
-
-        }
-
-
-    }
+				}
+			}
+			ViewBag.Message = "Wrong username or password";
+			return View();
+		}
+	}
 }
